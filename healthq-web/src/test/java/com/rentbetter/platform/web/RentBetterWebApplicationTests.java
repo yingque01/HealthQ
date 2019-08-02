8d5c45c407bcc078a3673fb1e8da73fc762e2ff8
yingpeng que <ying.q@rentbetter.com.au>
2019-08-02 22:07:55 +1000

    init commits WIP

---
+++ healthq-web/src/test/java/com/rentbetter/platform/web/RentBetterWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rentbetter.platform.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentBetterWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
