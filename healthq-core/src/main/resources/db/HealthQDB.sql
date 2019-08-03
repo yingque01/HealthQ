95a0981004ce9bd2a51143ebfb9caeb8c4d793c0
yingpeng que <ying.q@rentbetter.com.au>
2019-08-03 13:16:18 +1000

    update the referential actions on all FKs and add clinic UUIDs to the queue table def and the associated FK

--- healthq-core/src/main/resources/db/HealthQDB.sql
+++ healthq-core/src/main/resources/db/HealthQDB.sql
@@ -54,13 +54,14 @@ CREATE TABLE IF NOT EXISTS healthq.clinic_rooms
     unique key record_id (record_id),
     unique key clinic_rooms_id (id),
     constraint clinic_rooms_clinics_fk
-        foreign key (clinics_id) references healthq.clinics (id)
+        foreign key (clinics_id) references healthq.clinics (id) on update cascade on delete restrict
 );
 
 CREATE TABLE IF NOT EXISTS healthq.patient_queues
 (
 	record_id bigint(20) unsigned not null auto_increment,
     id varchar(36) collate utf8mb4_bin not null,
+    clinics_id varchar(36) collate utf8mb4_bin not null,
     clinic_rooms_id varchar(36) collate utf8mb4_bin not null,
     doctors_id varchar(36) collate utf8mb4_bin not null,
     started_at datetime default current_timestamp not null,
@@ -68,10 +69,12 @@ CREATE TABLE IF NOT EXISTS healthq.patient_queues
     primary key (record_id),
     unique key record_id (record_id),
     unique key patient_queues_id (id),
+    constraint patient_queues_clinics_fk
+        foreign key (clinics_id) references healthq.clinics (id) on update cascade on delete restrict,
     constraint patient_queues_clinic_rooms_fk
-        foreign key (clinic_rooms_id) references healthq.clinic_rooms (id),
+        foreign key (clinic_rooms_id) references healthq.clinic_rooms (id) on update cascade on delete restrictt,
    	constraint patient_queues_doctors_fk
-        foreign key (doctors_id) references healthq.doctors (id)    
+        foreign key (doctors_id) references healthq.doctors (id) on update cascade on delete restrict    
 );
 
 CREATE TABLE IF NOT EXISTS healthq.patient_queue_requests
@@ -86,7 +89,7 @@ CREATE TABLE IF NOT EXISTS healthq.patient_queue_requests
     unique key record_id (record_id),
     unique key patient_queue_requests_id (id),
     constraint patient_queue_requests_patients_fk
-        foreign key (patients_id) references healthq.patients (id),
+        foreign key (patients_id) references healthq.patients (id) on update cascade on delete restrict,
    	constraint patient_queue_requests_patient_queues_fk
-        foreign key (patient_queues_id) references healthq.patient_queues (id)    
+        foreign key (patient_queues_id) references healthq.patient_queues (id) on update cascade on delete restrict    
 );
